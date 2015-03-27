dd72aeee9940e8602a11ffe39fd85bb148ff65a6
lexxfirecore <lexxfirecore@gmail.com>
2015-03-27 21:45:29 +0200

    absolute path to Spring04

--- JavaBrains_SpringTutorial/src/main/java/com/lexxstudy/javabrains/spring04/writing_code_using_the_bean_factory/Main04.java
+++ JavaBrains_SpringTutorial/src/main/java/com/lexxstudy/javabrains/spring04/writing_code_using_the_bean_factory/Main04.java
@@ -3,6 +3,10 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.net.URL;
 
 
 public class Main04 {
@@ -11,12 +15,18 @@ public class Main04 {
 		//Triangle triangle = new Triangle();
 		System.out.println("Spring 04");
 
-        String path = "JavaBrains_SpringTutorial/src/main/java/com/lexxstudy/javabrains/spring04/writing_code_using_the_bean_factory/";
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource(path + "triangle04.xml"));
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource(getPath() + "triangle04.xml"));
 		Triangle triangle = (Triangle) factory.getBean("triangle");
 		
 		triangle.draw();
 
 	}
+
+    private static String getPath() {
+        URL location = Main04.class.getProtectionDomain().getCodeSource().getLocation();
+        String pathPrefix = location.getPath().replace("/target/classes","");
+        String pathSuffix = Main04.class.getPackage().getName().replace(".","/");
+        return pathPrefix + "src/main/java/" + pathSuffix + "/";
+    }
 
 }
