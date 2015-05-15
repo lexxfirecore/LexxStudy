374b2511c545dd1af0885b2d01eb28e5fb560310
lexxfirecore <lexxfirecore@gmail.com>
2015-05-15 17:31:08 +0300

    comments

--- JavaPractice/src/main/java/com/lexxstudy/spring_aop/common/MainControl.java
+++ JavaPractice/src/main/java/com/lexxstudy/spring_aop/common/MainControl.java
@@ -29,6 +29,16 @@ public class MainControl {
         amplifierServiceProxy.reduceGain(2);
         System.out.println("Amplifier value after aspect changes: " + amplifier.toString());
 
+                /* in loc de docs :)
+        o clasa amplifier si un service in care se injecteaza amplifier prin setter
+[17:27:59] Lexx: service schimba valorile din amplifier
+[17:28:26] Lexx: un aspect care face un proxy la service si citeste metodele apelate
+[17:28:51] Lexx: aspectul bean si proxy se declara tot in context
+[17:29:25] Lexx: in main chem proxy class de tip AmplifierService si chem ceva metode de la el
+[17:30:02] Lexx: si in acest moment se executa aspectul care modifica modelul amplului
+[17:30:04] Lexx: iaka asa
+         */
+
     }
 
 }
